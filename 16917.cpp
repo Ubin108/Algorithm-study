ea4751f275be8db1ad87cd8cfd72912268038791
Youbin <ybin0108@gmail.com>
2022-04-07 11:25:27 +0900

    boj 16917

---
+++ 16917.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int go(string &s, int index, char last) {
+	if (index == s.length()) {
+		return 1;
+	}
+
+	char start = (s[index] == 'c' ? 'a' : '0');
+	char end = (s[index] == 'd' ? 'z' : '9');
+	int ans = 0;
+	for (char i = start; i <= end; i++) {
+		if (i != last) {
+			ans += go(s, index + 1, i);
+		}
+	}
+	return ans;
+}
+
+
+int main() {
+	string s;
+	cin >> s;
+	go(s, 0, ' ');
+	return 0;
+}
