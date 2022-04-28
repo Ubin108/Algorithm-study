74dee8b7e94e1146ce5c1ebd27357e40b761a26c
김유빈 <50101568+Ubin108@users.noreply.github.com>
2022-04-28 23:03:51 +0900

    boj 16916-1
    라빈카프 알고리즘

--- 16916-1.cpp
+++ 16916-1.cpp
@@ -2,12 +2,12 @@
 #include <string>
 using namespace std;
 
-int mod = 127;
+long long mod = 2147483647; //큰 소수 사용하기
 int base = 256;
-int h(string s) {
-	int ans = 0;
+long long h(string s) {
+	long long ans = 0;
 	for (char ch : s) {
-		ans = (ans * base + ch) % mod;
+		ans = (ans * base + ch) % mod; //라빈카프 알고리즘
 	}
 	return ans;
 }
@@ -15,9 +15,9 @@ int go(string &s, string &p) {
 	int n = s.length();
 	int m = p.length();
 	if (n < m) return 0;
-	int hash_p = h(p);
-	int hash_s = h(s.substr(0, m));
-	int first = 1;
+	long long hash_p = h(p);
+	long long hash_s = h(s.substr(0, m));
+	long long first = 1;
 	for (int i = 0; i < m - 1; i++) {
 		first = (first * base) % mod;
 	}
@@ -25,7 +25,7 @@ int go(string &s, string &p) {
 		if (hash_p == hash_s) {
 			if (s.substr(i, m) == p) return 1;
 		}
-		if (i + m < n) {
+		if (i + m < n) { //이전 해쉬함수값으로 다음 해쉬함수값 구하기
 			hash_s = hash_s - (s[i]*first)%mod;
 			hash_s = (hash_s + mod) % mod;
 			hash_s = ((hash_s * base) % mod + s[i + m]) % mod;
